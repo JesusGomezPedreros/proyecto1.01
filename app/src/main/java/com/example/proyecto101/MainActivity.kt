bd3cc2c717b01618d68115cf6c413b98a2ea22c5
JesusGomezPedreros <56317446+JesusGomezPedreros@users.noreply.github.com>
2021-09-23 23:55:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/proyecto101/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.proyecto101
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+		Botones()
+
+	}
+	//Funcion que llama los botones
+	fun Botones(){
+		RegisterMaterial.setOnClickListener {
+			val intent:Intent = Intent(this, registroDeMateriales::class.java)
+				startActivity(intent)
+			finish()
+		}
+	}
+}
---
+++ app/src/main/java/com/example/proyecto101/registroDeMateriales.kt
@@ -0,0 +1,41 @@
+package com.example.proyecto101
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.proyecto101.databinding.ActivityRegistroDeMaterialesBinding
+import kotlinx.android.synthetic.main.activity_registro_de_materiales.*
+
+class registroDeMateriales : AppCompatActivity() {
+	//binding crea actividad para menu desplegable
+	private lateinit var binding: ActivityRegistroDeMaterialesBinding
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = ActivityRegistroDeMaterialesBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+
+		botones ()
+		menuDesplegable()
+	}
+	//Funcion que llama los botones
+	fun botones (){
+		startWindows.setOnClickListener {
+			val intent:Intent = Intent(this, MainActivity::class.java)
+			startActivity(intent)
+		}
+	}
+
+	fun menuDesplegable(){
+		val materialsGather = resources.getStringArray(R.array.materials_gather)
+		val adapter = ArrayAdapter(
+			this,
+			R.layout.list_item,
+			materialsGather
+		)
+		with(binding.autoCompleteTextView){
+			setAdapter(adapter)
+		}
+	}
+}
